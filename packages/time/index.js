c1e72f941de7f4f59c082e6d16d6b189ad822ab2
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-03-23 01:22:28 -0400

    time: add CORS headers

--- packages/time/index.js
+++ packages/time/index.js
@@ -1,6 +1,17 @@
 module.exports = (req, res) => {
   const time = new Date().toISOString()
+
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Request-Method', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
   res.setHeader('Content-Type', 'application/json')
-  // res.send(200, JSON.stringify({ time: time }))
   res.end(JSON.stringify({ time: time }))
 }
