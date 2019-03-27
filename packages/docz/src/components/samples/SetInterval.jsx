4eaef4f166c5fc4514eff4114bea865bb3bb8b54
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-03-27 19:35:06 -0400

    docz: SetInterval

--- packages/docz/src/components/samples/SetInterval.jsx
+++ packages/docz/src/components/samples/SetInterval.jsx
@@ -8,53 +8,31 @@ import React from 'react'
 export default class SetInterval extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { ...props.initialState }
+    this.state = { stamp: new Date().toISOString() }
   }
-  componentDidMount () {
-    console.log('---', this.state, this.props)
 
-    const { done, state } = this.props.tick(this.state)
-    this.setState(state)
-    console.log('one', this.state)
-    if (!done) {
+  componentDidMount () {
+    if (!this.props.once) {
       this.interval = setInterval(() => {
-        // const counter = this.state.counter + 1
-        const { done, state } = this.props.tick(this.state)
-        if (done) {
-          console.log('clr', this.state)
-          clearInterval(this.interval)
-          delete this.interval
-        } else {
-          this.setState(state)
-          console.log('inc', this.state)
-        }
-        console.log('===', this.state)
+        this.setState({ stamp: new Date().toISOString() })
       }, this.props.delay)
     }
   }
+
   componentWillUnmount () {
     clearInterval(this.interval)
   }
+
   render () {
-    // return this.props.render({ counter: this.state.counter })
     return this.props.render(this.state)
   }
 }
+
 SetInterval.defaultProps = {
   delay: 1000,
-  initialState: { counter: 0 },
-  // tick: (state) => ({ done: true, state }),
-  tick: (state) => ({
-    // done: Math.random() < 0.1,
-    // done: state.counter >= 3,
-    // done: false,
-    done: true,
-    state: { counter: state.counter + 1 }
-  }),
-  iterations: -1,
-  // render: (state) => (null)
+  once: false,
   render: (state) => {
-    console.log('render', { state })
-    return (<div>default-o-rama:{JSON.stringify(state)}</div>)
+    // console.log('render', state)
+    return (<pre>default-o-rama:{JSON.stringify(state)}</pre>)
   }
 }
