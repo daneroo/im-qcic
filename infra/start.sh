720b3837429a838e40d9c5d8d9f15fcfd8ce19bc
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-03-29 16:59:44 -0400

    infra: open ui.qcic.n.imetrical.com

--- infra/start.sh
+++ infra/start.sh
@@ -10,6 +10,8 @@ npx ttab -t status-cron -d "$(pwd)/compose" ./dc.sh logs -f status
 
 npx ttab -t ddclient -d "$(pwd)/compose" ./dc.sh logs -f ddclient
 
+open https://ui.qcic.n.imetrical.com/
+
 # show components and status
 (cd compose; ./dc.sh ps)
 
