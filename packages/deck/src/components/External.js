a62ce060b9806d90da4fd29316dd45a28a73ea24
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-03-31 12:11:35 -0400

    deck:Test @pika/pack external library in deck

---
+++ packages/deck/src/components/External.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+// import gql from 'graphql-tag'
+// import { df } from './df'
+// import { Subscription } from 'react-apollo'
+// import { RegisterApolloProvider } from './gql/RegisterApolloProvider'
+import { Greeter, Welcome, Wilkomen } from '@daneroo/qcic-apollo-client-setup'
+
+// const natsSubscription = gql`
+// subscription OnNewMessage {
+//   newMessage {
+//     id
+//     stamp
+//     host
+//     text
+//   }
+// }`
+
+export default function External () {
+  return <div style={{ fontSize: '50%' }}>
+    <h4>Greeter: {Greeter('Dan-da-man')}</h4>
+    <Welcome />
+    <Wilkomen />
+  </div>
+}
