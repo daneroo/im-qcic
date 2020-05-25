c0500c5496107937708138cdb8424348224b5bcd
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-05-24 21:43:45 -0400

    status: update node and npm packages

--- packages/status/src/index.test.js
+++ packages/status/src/index.test.js
@@ -4,7 +4,7 @@ describe('@daneroo/qcic-status', () => {
   describe('Smoke test', () => {
     test.each([
       // [name, expected, v],
-      [ 'true', true, true ]
+      ['true', true, true]
     ])('Check truth (%s)', (name, expected, v) => {
       expect(v).toBe(expected)
     })
