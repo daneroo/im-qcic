7ee05919013dba2791aa8f050d808ec5ce6908aa
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-06-06 16:40:37 -0400

    site: initial commit with package-lock

---
+++ packages/site/README.md
@@ -0,0 +1,32 @@
+# `@daneroo/qcic-site`
+
+Main static site for QCIC
+
+## TODO
+
+- stole package-lock.json from site-fresh, and uninstalled things
+
+## Usage
+
+### Develop
+
+```bash
+npm start
+# - or -
+gatsby develop
+```
+
+
+### Publish (deploy)
+
+## Setup
+
+This is a gatsby site using  the Document theme  by [Code Bushi](https://codebushi.com/gatsby-starters-and-themes/).
+
+Now add dependencies
+```bash
+npm i -D gatsby gatsby-theme-document 
+```
+
+- Adjust `ghatsby-config`
+  - 
---
+++ packages/site/gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: 'QCIC',
+    name: 'QCIC',
+    siteUrl: 'https://qcic.n.imetrical.com/',
+    description: 'Quis custodiet ipsos custodes - Who will watch the watchers',
+    social: [
+      {
+        name: 'github',
+        url: 'https://github.com/daneroo'
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/daneroo'
+      }
+    ],
+    sidebarConfig: {
+      forcedNavOrder: ['/introduction'],
+      ignoreIndex: true
+    }
+  },
+  plugins: [{ resolve: 'gatsby-theme-document' }]
+}
