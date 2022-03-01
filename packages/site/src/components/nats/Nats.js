4574cec17ae67469ea2075cfa30a5fa04849bb90
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-03-01 14:06:06 -0500

    Update nats ws

--- packages/site/src/components/nats/Nats.js
+++ packages/site/src/components/nats/Nats.js
@@ -4,6 +4,7 @@ import { df } from '../df'
 
 export function Subscribe ({
   wsurl = 'wss://nats.dl.imetrical.com',
+  // wsurl = 'ws://nats.ts.imetrical.com:9222',
   subject = 'im.qcic.heartbeat',
   maxRows = 5
 }) {
@@ -36,25 +37,30 @@ function useSubscribe ({ wsurl, subject, maxRows, messages, setMessages }) {
 
   useEffect(() => {
     async function connectToNats () {
-      console.log(`Connect to: ${wsurl}`)
-      const nc = await connect({
-        name: 'react-nats.ws',
-        servers: wsurl,
-        pendingLimit: 8192
-      })
-      ncRef.current = nc
+      try {
+        console.log(`Connecting to: ${wsurl}`)
+        const nc = await connect({
+          name: 'react-nats.ws',
+          servers: wsurl
+          // pendingLimit: 8192
+        })
+        console.log(`Connected to: ${wsurl}`)
+        ncRef.current = nc
 
-      const jc = JSONCodec()
-      console.log(`Subscribe to: ${subject}`)
-      const sub = nc.subscribe(subject, {})
-      subRef.current = sub
-      setTimeout(async () => {
-        for await (const m of sub) {
-          const jm = jc.decode(m.data)
-          // console.log(JSON.stringify(jm))
-          setMessagesRef.current([...messagesRef.current, jm].slice(-maxRows))
-        }
-      }, 0)
+        const jc = JSONCodec()
+        console.log(`Subscribe to: ${subject}`)
+        const sub = nc.subscribe(subject, {})
+        subRef.current = sub
+        setTimeout(async () => {
+          for await (const m of sub) {
+            const jm = jc.decode(m.data)
+            // console.log(JSON.stringify(jm))
+            setMessagesRef.current([...messagesRef.current, jm].slice(-maxRows))
+          }
+        }, 0)
+      } catch (err) {
+        console.error(err)
+      }
     }
     connectToNats()
 
@@ -71,11 +77,13 @@ function useSubscribe ({ wsurl, subject, maxRows, messages, setMessages }) {
 // eslint-disable-next-line no-unused-vars
 function MessagesLayout ({ messages, maxRows = 0 }) {
   if (!messages) return <p>---</p>
-  const headers = (messages.length) ? Object.keys(messages[0]) : ['stamp', 'host', 'text']
+  const headers = messages.length
+    ? Object.keys(messages[0])
+    : ['stamp', 'host', 'text']
   const rows = [
     headers,
     ...messages
-      .map((msg) => {
+      .map(msg => {
         const { stamp, host, text } = msg
         const row = [df(stamp, 'HH:mm:ss'), host, text]
         return row
